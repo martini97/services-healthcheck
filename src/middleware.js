5ed2adff06a3018cb577c9c23d567768fe3adb0f
martini97 <alessandro.martini@bravi.com.br>
2017-08-11 13:20:44 -0300

    Remove async and add function wrapper
    Removed an unnecessary async from a function and add a wrapper for
    catching async error

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,13 @@
 import healthCheck from './healthcheck';
 
+const wrap = fn => (...args) => fn(...args).catch(args[2]);
+
 // eslint-disable-next-line consistent-return
-export default async services => async (req, res, next) => {
+export default services => wrap(async (req, res, next) => {
   if (req.url !== '/_health' || req.method !== 'GET') {
     return next();
   }
 
   const health = await healthCheck(services);
   res.status(200).send(health);
-};
+});
