95c31da2e104ba9add8f3866f621d61ccfecda61
Alessandro Martini <alessandrofmartini@gmail.com>
2017-08-15 15:51:31 -0300

    Revert "Add health-check to amqp"

--- src/middleware.js
+++ src/middleware.js
@@ -4,11 +4,10 @@ const wrap = fn => (...args) => fn(...args).catch(args[2]);
 
 /**
  * middleware returns a middleware function to be used by express.
- * @param {Object} services - a map of the services to their values
+ * @params {Object} services - a map of the services to their values
  * (see getService [healthcheck.js])
  * @returns {function} - a function to be used as a middleware on express.
  */
-
 // eslint-disable-next-line consistent-return
 export default services => wrap(async (req, res, next) => {
   if (req.url !== '/_health' || req.method !== 'GET') {
--- src/ping.js
+++ src/ping.js
@@ -1,24 +1,18 @@
 import axios from 'axios';
 
 import databaseIsUp from './database';
-import queueIsUp from './queue';
 
 /**
  * ping connects with the service passed and returns it's status.
- * @param {Object} - if there is an url, it will ping it on the route /_ping or
+ * @params {Object} - if there is an url, it will ping it on the route /_ping or
  * on the route that was passed and return it's status. If there is a knex it
  * will try to connect to the database, and return the connection status.
  * @returns {Object} - the status code of the connection, and the error code if
  * there was one.
  */
-export default async ({ url, route, knex, queue }) => {
+export default async ({ url, route, knex }) => {
   if (knex) {
     const isUp = await databaseIsUp(knex);
-    return { status: isUp ? 200 : 500 };
-  }
-
-  if (queue) {
-    const isUp = await queueIsUp(queue);
     return { status: isUp ? 200 : 500 };
   }
 
