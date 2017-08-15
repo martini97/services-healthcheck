2e86e342d39f1bea177ca74727c750c28409d6e3
martini97 <alessandro.martini@bravi.com.br>
2017-08-15 15:46:39 -0300

    Refactor tests

--- test/database.spec.js
+++ test/database.spec.js
@@ -1,31 +1,18 @@
 import { feature } from 'ava-spec';
-import knex from 'knex';
-import mockKnex from 'mock-knex';
 
 import database from '../lib/database';
-
-const knexUp = knex({
-  client: 'mysql',
-  connection: {
-    host: 'localhost',
-    user: 'user',
-    password: 'password',
-    database: 'database',
-    port: 3306,
-  },
-  debug: false,
-});
-
-const knexDown = knex({
-  client: 'mysql',
-});
+import {
+  knexUp,
+  knexDown,
+  mockKnex,
+  knexTracker,
+} from './consts';
 
 feature('testing database connection', scenario => {
   scenario('given that the database is up', async t => {
-    const tracker = mockKnex.getTracker();
     mockKnex.mock(knexUp);
-    tracker.install();
-    tracker.on('query', query => {
+    knexTracker.install();
+    knexTracker.on('query', query => {
       query.response();
     });
 
--- test/queue.spec.js
+++ test/queue.spec.js
@@ -1,15 +1,13 @@
 import { feature } from 'ava-spec';
 
 import queue from '../lib/queue';
-
-const amqpDownConfig = { host: 'localhost', port: '5680' };
-const amqpUpConfig = { host: 'localhost', port: '5672' };
+import {
+  amqpUpConfig,
+  amqpDownConfig,
+} from './consts';
 
 feature('testing queue connection', scenario => {
   scenario.skip('given that the queue is up', async t => {
-    // I haven't found a way to mock the queue yet, so unless you have it running
-    // skip this test. If you have docker and want to run this test:
-    // $ docker run -d --name amqp.test -p 5672:5672 rabbitmq
     const isUp = await queue(amqpUpConfig);
     t.true(isUp);
   });
